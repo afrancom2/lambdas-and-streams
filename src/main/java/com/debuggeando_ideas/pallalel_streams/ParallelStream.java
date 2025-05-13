badc050244c7487d420f52c50ee0826cc580cead
Andres Franco <afrancom2@gmail.com>
2025-05-13 00:28:59 -0500

    add parallel streams

--- src/main/java/com/debuggeando_ideas/pallalel_streams/ParallelStream.java
+++ src/main/java/com/debuggeando_ideas/pallalel_streams/ParallelStream.java
@@ -6,6 +6,7 @@ public class ParallelStream {
 
     //-D java.util.concurrent.ForkJoinPool.common.parallelism=5
     public static void main(String[] args) {
-
+        List<Integer> numbers = List.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        numbers.parallelStream().forEach(number -> System.out.println(number + " - " + Thread.currentThread().getName()));
     }
 }
